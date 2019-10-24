4635355e50396840ac85dcd41ec9d1526a30b7f1
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-24 13:56:25 +0200

    Initial commit

---
+++ src/lib-components/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+export { default as CityObjectCard } from './CityObjectCard.vue';
+export { default as CityObjectsTree } from './CityObjectsTree.vue';
+export { default as CityObjectsTreeItem } from './CityObjectsTreeItem.vue';
+export { default as ThreeJsViewer } from './ThreeJsViewer.vue';
---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { CityjsonVueComponentsSample } from './entry';
+export default {
+    name:  'CityjsonVueComponentsLibDev', // vue library dev component
+    components: {
+        CityjsonVueComponentsSample,
+    },
+}
+</script>
+
+<template>
+    <div class="cityjson-vue-components-lib-dev">
+        <cityjson-vue-components-sample></cityjson-vue-components-sample>
+    </div>
+</template>
+
+<style scoped>
+    .cityjson-vue-components-lib-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
