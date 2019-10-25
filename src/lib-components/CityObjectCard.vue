3c679d0c02beef94438445f1ec11353f001ff2ff
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-25 09:46:44 +0200

    Redesign the CityObjectCard
    Header was removed and a more clean and colorful design was put into
    place. There is also an extra tab for geometries.

--- src/lib-components/CityObjectCard.vue
+++ src/lib-components/CityObjectCard.vue
@@ -1,49 +1,65 @@
 <template>
   <div class="card mb-2" :id="cityobject_id" :class="{ 'border-primary' : selected }">
-    <div class="card-header">
+    <div class="card-body">
       <div class="d-flex justify-content-between">
-        <div class="text-truncate">
-          <a href="#" id="objicon" :title="cityobject.type"><i v-bind:class="iconType"></i></a> <a href="#" @click="select_this" class="text-dark text-decoration-none mr-5" >{{ cityobject_id }}</a>
-        </div>
+        <div class="font-weight-bold text-primary text-uppercase mb-2">{{ cityobject.type }}</div>
         <div class="col-auto p-0">
-          <a href="#" @click="expanded = !expanded" v-if="hasAttributes"><i class="fas text-secondary mr-1" :class="[ expanded ? 'fa-minus' : 'fa-plus' ]"></i></a>
-          <a href="#" @click="edit_mode = !edit_mode"><i class="fas fa-pen mr-1" :class="[ edit_mode ? 'text-dark' : 'text-secondary' ]"></i></a>
+          <button class="btn btn-sm" :class="[ edit_mode ? 'btn-warning' : 'btn-outline-warning' ]" @click="edit_mode = !edit_mode"><i class="fas fa-pen mr-1"></i> {{ edit_mode ? 'Close edit' : 'Edit' }}</button>
         </div>
       </div>
-      <small v-show="'parents' in cityobject">
-        Parents:
-        <a
-          v-for="parent_id in cityobject.parents"
-          :key="parent_id"
-          :href="'#' + parent_id"
-          :title="parent_id">
-            <i class="text-danger" :class="getIconStyle(getObject(parent_id), false)"></i>
-        </a>
-      </small>
-      <small v-show="'children' in cityobject">
-        Children:
-        <a
-          v-for="child_id in cityobject.children"
-          :key="child_id"
-          :href="'#' + child_id"
-          :title="child_id">
-            <i class="text-success" :class="getIconStyle(getObject(child_id), false)"></i>
-        </a>
-      </small>
-    </div>
-    <div class="card-body collapse show" :id="cityobject_id + 'Body'" v-if="hasAttributes && expanded || edit_mode">
-      <table class="table table-striped table-borderless overflow-auto" v-show="edit_mode == false">
-        <tbody>
-          <tr v-for="(value, key) in cityobject.attributes" :key="key">
-            <th scope="row" class="py-1"><small class="font-weight-bold">{{ key }}</small></th>
-            <td class="py-1"><small>{{ value }}</small></td>
-          </tr>
-        </tbody>
-      </table>
-      <div v-if="edit_mode">
-        <textarea id="json_data" class="form-control" v-model="jsonString"></textarea>
-        <div class="d-flex justify-content-end mt-2">
-          <button type="button" class="btn btn-success btn-sm" @click="saveChanges">Save</button>
+      <h5 class="card-title text-truncate">
+        {{ cityobject_id }}
+      </h5>
+      <div>
+        <small v-show="'parents' in cityobject">
+          Parents:
+          <a
+            v-for="parent_id in cityobject.parents"
+            :key="parent_id"
+            :href="'#' + parent_id"
+            :title="parent_id">
+              <i class="text-danger" :class="getIconStyle(getObject(parent_id), false)"></i>
+          </a>
+        </small>
+        <small v-show="'children' in cityobject">
+          Children:
+          <a
+            v-for="child_id in cityobject.children"
+            :key="child_id"
+            :href="'#' + child_id"
+            :title="child_id">
+              <i class="text-success" :class="getIconStyle(getObject(child_id), false)"></i>
+          </a>
+        </small>
+      </div>
+      <div class="d-flex mt-2">
+        <div class="badge badge-pill badge-info mr-2" v-if="hasAttributes">
+          <a href="#" class="text-white text-decoration-none" @click="toggle_mode(1)" v-if="hasAttributes">{{ attributesCount }} Attributes <i class="fas ml-1" :class="[ !edit_mode && is_mode(1) ? 'fa-chevron-up' : 'fa-chevron-down' ]"></i></a>
+        </div>
+        <div class="badge badge-pill badge-danger mr-2" v-if="hasAttributes">
+          <a href="#" class="text-white text-decoration-none" @click="toggle_mode(2)" v-if="hasGeometries">{{ this.cityobject.geometry.length }} Geometries <i class="fas ml-1" :class="[ !edit_mode && is_mode(2) ? 'fa-chevron-up' : 'fa-chevron-down' ]"></i></a>
+        </div>
+      </div>
+      <div v-if="hasAttributes && expanded || edit_mode">
+        <hr>
+        <table class="table table-striped table-borderless overflow-auto" v-show="edit_mode == false && is_mode(1)" >
+          <tbody>
+            <tr v-for="(value, key) in cityobject.attributes" :key="key">
+              <th scope="row" class="py-1"><small class="font-weight-bold">{{ key }}</small></th>
+              <td class="py-1"><small>{{ value }}</small></td>
+            </tr>
+          </tbody>
+        </table>
+        <div v-show="edit_mode == false && is_mode(2)">
+          <ul>
+            <li v-for="(geom, i) in cityobject.geometry" :key="i">{{ geom.type }}</li>
+          </ul>
+        </div>
+        <div v-if="edit_mode">
+          <textarea id="json_data" class="form-control" v-model="jsonString"></textarea>
+          <div class="d-flex justify-content-end mt-2">
+            <button type="button" class="btn btn-success btn-sm" @click="saveChanges"><i class="fas fa-save mr-1"></i> Save</button>
+          </div>
         </div>
       </div>
     </div>
@@ -63,8 +79,8 @@ export default {
       default: false
     },
     expanded: {
-      type: Boolean,
-      default: false
+      type: Number,
+      default: 0
     }
   },
   data() {
@@ -73,8 +89,14 @@ export default {
     }
   },
   computed: {
+    attributesCount: function() {
+      return Object.keys(this.cityobject.attributes).length;
+    },
     hasAttributes: function() {
-      return "attributes" in this.cityobject && Object.keys(this.cityobject.attributes).length > 0;
+      return "attributes" in this.cityobject && this.attributesCount > 0;
+    },
+    hasGeometries: function() {
+      return this.cityobject.geometry;
     },
     iconType: function() {
       return this.getIconStyle(this.cityobject);
@@ -86,11 +108,31 @@ export default {
     }
   },
   methods: {
+    toggle_mode(mode) {
+      if (this.expanded == mode)
+      {
+        this.expanded = 0;
+      }
+      else
+      {
+        this.expanded = mode;
+      }
+    },
+    is_mode(mode) {
+      return this.expanded == mode;
+    },
     select_this() {
       this.$parent.$emit('object_clicked', this.cityobject_id);
     },
     getObject(objid) {
-      return this.$parent.citymodel.CityObjects[objid];
+      if (this.$parent.citymodel)
+      {
+        return this.$parent.citymodel.CityObjects[objid];
+      }
+      else
+      {
+        return {};
+      }
     },
     getIconStyle(cityobj, with_colour=true) {
       var type_icons = {
@@ -134,9 +176,9 @@ export default {
     saveChanges() {
       var card_id = $.escapeSelector(this.cityobject_id);
       var new_json = $(`#${card_id} #json_data`).val();
-      this.cityobject = JSON.parse(new_json);
+      var new_cityobject = JSON.parse(new_json);
 
-      this.$emit('input', this.cityobject);
+      this.$emit('input', new_cityobject);
     }
   }
 }
